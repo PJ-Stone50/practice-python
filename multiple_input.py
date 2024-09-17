8ff12b5c70b1bc380709d16f8bf3a0c42ea2500f
PJ-Stone50 <pj.webdeveloper01@gmail.com>
2024-09-17 19:40:22 +0700

    multiple input

---
+++ multiple_input.py
@@ -0,0 +1,8 @@
+# # Using List Comprehension
+# num_inputs = int(input('Enter the number of inputs: '))
+# user_inputs = [input('Enter input {i + 1}: ') for i in range(num_inputs)]
+
+# print('User inputs:', user_inputs)
+
+test_list = [int(input("Enter number")) for i in range(5)]
+print(test_list)
